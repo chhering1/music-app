fec4527656ca6e9a4b1b7566adb466396e923cde
chhering1 <6088255@rts-365.dk>
2021-02-05 09:24:19 +0100

    7 songs deleted

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,25 +6,15 @@ import dj from '../media/Dj.mp3';
 import she from '../media/She.mp3';
 import genda from '../media/Genda.mp3';
 import mercy from '../media/Mercy.mp3';
-import anthem from '../media/anthem.mp3';
 import basanti from '../media/basanti.mp3';
 import bulleya from '../media/bulleya.mp3';
-import dance from '../media/dance.mp3';
-import drama from '../media/drama.mp3';
 import ghungroo from '../media/ghungroo.mp3';
-import kurbaan from '../media/kurbaan.mp3';
-import luvstory from '../media/luvstory.mp3';
 import mileya from '../media/mileya.mp3';
 import nashe from '../media/nashe.mp3';
 import nayan from '../media/Nayan.mp3';
 import patola from '../media/patola.mp3';
 import radha from '../media/radha.mp3';
-import tumeri from '../media/tumeri.mp3';
 import vele from '../media/vele.mp3';
-import we from '../media/we.mp3';
-
-
-
 import Localbase from 'localbase'
 import 'react-h5-audio-player/lib/styles.css';
 import Collapsible from 'react-collapsible'
@@ -104,11 +94,7 @@ const Home = () => {
 
         <Collapsible trigger="🎵 Vishal-dadlani songs" className="collapse">
 
-        <div className="songs-lists ">
-            <h4>Anthem</h4>
-          <button onClick={addNewSong}  data-songname='Anthem' data-songurl={anthem}>Add to playlist</button>
-          <AudioPlayer   src={anthem} />
-        </div>
+        
         <div className="songs-lists ">
             <h4>Basanti</h4>
           <button onClick={addNewSong}  data-songname='basanti' data-songurl={basanti}>Add to playlist</button>
@@ -119,31 +105,15 @@ const Home = () => {
           <button onClick={addNewSong}  data-songname='Bulleya' data-songurl={bulleya}>Add to playlist</button>
           <AudioPlayer   src={bulleya} />
         </div>
-        <div className="songs-lists ">
-            <h4>Dance</h4>
-          <button onClick={addNewSong}  data-songname='dance' data-songurl={dance}>Add to playlist</button>
-          <AudioPlayer   src={dance} />
-        </div>
-        <div className="songs-lists ">
-            <h4>Drama</h4>
-          <button onClick={addNewSong}  data-songname='drama' data-songurl={drama}>Add to playlist</button>
-          <AudioPlayer   src={drama} />
-        </div>
+        
+       
         <div className="songs-lists ">
             <h4>Ghungroo</h4>
           <button onClick={addNewSong}  data-songname='ghungroo' data-songurl={ghungroo}>Add to playlist</button>
           <AudioPlayer   src={ghungroo} />
         </div>
-        <div className="songs-lists ">
-            <h4>Kurbaan</h4>
-          <button onClick={addNewSong}  data-songname='kurbaan' data-songurl={kurbaan}>Add to playlist</button>
-          <AudioPlayer   src={kurbaan} />
-        </div>
-        <div className="songs-lists">
-            <h4>Love story</h4>
-          <button onClick={addNewSong}  data-songname='luvstory' data-songurl={luvstory}>Add to playlist</button>
-          <AudioPlayer   src={luvstory} />
-        </div>
+        
+       
         <div className="songs-lists">
             <h4>Mileya</h4>
           <button onClick={addNewSong}  data-songname='mileya' data-songurl={mileya}>Add to playlist</button>
@@ -169,21 +139,13 @@ const Home = () => {
           <button onClick={addNewSong}  data-songname='radha' data-songurl={radha}>Add to playlist</button>
           <AudioPlayer   src={radha} />
         </div>
-        <div className="songs-lists ">
-            <h4>Tu meri</h4>
-          <button onClick={addNewSong}  data-songname='tumeri' data-songurl={tumeri}>Add to playlist</button>
-          <AudioPlayer   src={tumeri} />
-        </div>
+        
         <div className="songs-lists ">
             <h4>Vele</h4>
           <button onClick={addNewSong}  data-songname='vele' data-songurl={vele}>Add to playlist</button>
           <AudioPlayer   src={vele} />
         </div>
-        <div className="songs-lists">
-            <h4>We</h4>
-          <button onClick={addNewSong}  data-songname='we' data-songurl={we}>Add to playlist</button>
-          <AudioPlayer   src={we} />
-        </div>
+        
 
         </Collapsible>
           
